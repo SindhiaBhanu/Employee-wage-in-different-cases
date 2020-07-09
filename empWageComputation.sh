00099543b6818ba8f47ef1add078f2eed0a7d87a
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-09 21:13:10 +0530

    added employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,43 +1,10 @@
 #!/bin/bash -x
-isFullTime=2
-isPartTime=1
-maxHrsInMonth=10
-ratePerHour=20
-noOfWorkingDays=20
+isPresent=1
+employeeCheck=$((RANDOM%2))
 
-totalEmpHrs=0
-totalWorkingDays=0;
-
-declare -A dailyWage
-
-function getWorkHrs(){
-        local empCheck=$1
-        case $empCheck in
-                $isFullTime)
-                        empHrs=8;;
-                $isPartTime)
-                        empHrs=4;;
-                *)
-                        empHrs=0;;
-        esac
-        echo $empHrs
-        #totalEmpHrs=$(($totalEmpHrs+$empHrs))
-}
-
-function getEmpWage () {
-        local empHr=$1
-        echo $(($empHr*$ratePerHour))
-}
-while [[ $totalEmpHrs -lt $maxHrsInMonth
-                                 && $totalWorkingDays -lt $noOfWorkingDays ]]
-do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
-        empHrs="$( getWorkHrs $empCheck )"
-        totalEmpHrs=$(($totalEmpHrs+$empHrs))
-        dailyWage["Day "$totalWorkingDays]="$( getEmpWage $empHrs )"
-done
-
-totalSalary=$(($totalEmpHrs*$ratePerHour))
-echo ${dailyWage[@]}
-echo ${!dailyWage[@]}
+if [ $isPresent -eq $employeeCheck ];
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
